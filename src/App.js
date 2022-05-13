f8801233ee0498146a873907c4043a4554ac2282
hamidk144 <hamid.k144@gmail.com>
2022-05-13 15:26:39 +0500

    intial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     {/* All components has been called in Index.js */}
+    </div>
+  );
+}
+
+export default App;
